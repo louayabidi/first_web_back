56d11bf22a7df210fe013645bc1ccf02cce98bb4
louayabidi <louay.abidi@esprit.tn>
2025-10-26 20:15:39 +0100

    deployyEMENT

--- index.js
+++ index.js
@@ -263,9 +263,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Atlas connected");
-    const port = process.env.PORT || 5000;
    const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`🚀 Backend running on port ${port}`));
+  app.listen(port, () => console.log(`🚀 Backend running on port ${port}`));
 
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
