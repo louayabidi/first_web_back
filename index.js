6d855a1121b23da00b5269e98c428ede740790e1
louayabidi <louay.abidi@esprit.tn>
2025-10-26 20:12:54 +0100

    deploy

--- index.js
+++ index.js
@@ -256,10 +256,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/images", imagesRouter);
 
 // --- SERVE REACT FRONTEND ---
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
 
 
 // --- CONNECT TO MONGODB AND START SERVER ---
@@ -268,8 +264,8 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB Atlas connected");
     const port = process.env.PORT || 5000;
-    app.listen(port, () =>
-      console.log(`🚀 Backend running on http://localhost:${port}`)
+    app.listen(port, () => console.log(`🚀 Backend running on port ${port}`));
+
     );
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
